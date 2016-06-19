b9da7def7c0c084fa61418be7770e8bff96d86fa
Tony Bowden <tony@tmtm.com>
2016-06-19 11:12:17 +0100

    Also by Category

--- scraper.rb
+++ scraper.rb
@@ -4,5 +4,10 @@
 require 'wikidata/fetcher'
 
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/azerbaijan-national-assembly-wikipedia', column: 'wikiname')
-EveryPolitician::Wikidata.scrape_wikidata(names: { az: names }, output: false)
+
+cat_5 = WikiData::Category.new( 'Kateqoriya:Azərbaycan Respublikası Milli Məclisinin V çağırış deputatları', 'az').member_titles
+cat_4 = WikiData::Category.new( 'Kateqoriya:Azərbaycan Respublikası Milli Məclisinin IV çağırış deputatları', 'az').member_titles
+
+
+EveryPolitician::Wikidata.scrape_wikidata(names: { az: names | cat_4 | cat_5 }, output: false)
 
