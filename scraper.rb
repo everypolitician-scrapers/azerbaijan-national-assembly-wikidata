f40261b79b9a51270d07b07510ea3ac739fc5344
Tony Bowden <tony@tmtm.com>
2018-03-16 07:30:14 +0000

    Also fetch via Wikidata P39s

--- scraper.rb
+++ scraper.rb
@@ -8,6 +8,14 @@ names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/azerbaijan-n
 cat_5 = WikiData::Category.new( 'Kateqoriya:Azərbaycan Respublikası Milli Məclisinin V çağırış deputatları', 'az').member_titles
 cat_4 = WikiData::Category.new( 'Kateqoriya:Azərbaycan Respublikası Milli Məclisinin IV çağırış deputatları', 'az').member_titles
 
+# Members of the 4th/5th Convocation
+sparq = <<EOQ
+  SELECT DISTINCT ?item WHERE {
+    VALUES ?term { wd:Q50595190 wd:Q28527720 }
+    ?item p:P39 [ ps:P39 wd:Q21269547 ; pq:P2937 ?term ]
+  }
+EOQ
+ids = EveryPolitician::Wikidata.sparql(sparq)
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { az: names | cat_4 | cat_5 }, output: false)
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { az: names | cat_4 | cat_5 })
 
